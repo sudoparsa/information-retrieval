eb48fedbbc7ce6dadbe31047bae5a933b73bf32a
sudoparsa <sp.hoseiny@gmail.com>
2022-07-21 23:01:21 +0430

    add clustering code

---
+++ code/clustering.py
@@ -0,0 +1,64 @@
+import json
+import time
+
+from sklearn.cluster import k_means
+from preprocess import Preprocess
+import numpy as np
+from sentence_transformers import SentenceTransformer, util
+import pickle
+
+
+class KmeansClustering:
+    def __init__(self,
+                 model_path='models/SentenceTransformers/model/',
+                 embedding_path='models/Clustering/doc_embedding.json',
+                 kmeans_path='models/Clustering/kmeans.pkl',
+                 result_path='models/Clustering/result.json'):
+        self.model = SentenceTransformer(model_path)
+        self.doc_embedding = json.load(open(embedding_path))
+        self.kmeans = pickle.load(open(kmeans_path, "rb"))
+        self.clustering_metrics = json.load(open(result_path))
+        self.preprocessor = Preprocess()
+    
+    def show(self, indexes, scores, cluster_embeddings):
+        print('\n')
+        print('Similar Papers using Cosine Similarity:')
+        for ix, i in zip(indexes, range(len(indexes))):
+            print(f'\n{i}.', end='')
+            print(f' {cluster_embeddings["title"][ix]}')
+            print(f'Label : {cluster_embeddings["label"][ix]}')
+            print(f'Cosine Similarity : {scores[ix]}')
+        print()
+    
+
+    def get_cluster_embeddings(self, cluster):
+        ix = []
+        for i, emb in enumerate(self.doc_embedding['embedding']):
+            if cluster == self.kmeans.predict(np.array(list(map(np.float32, emb))).reshape(1, -1)):
+                ix.append(i)
+        r = {}
+        for key in self.doc_embedding.keys():
+            r[key] = [self.doc_embedding[key][i] for i in ix]
+        return r
+
+    def most_similar(self, query, k):
+        query = self.preprocessor.run(query)
+        query_embedding = self.model.encode(query)
+        query_cluster = self.kmeans.predict(query_embedding.reshape(1, -1))
+        cluster_embeddings = self.get_cluster_embeddings(query_cluster)
+        embeddings = cluster_embeddings['embedding']
+        cosine_scores = util.dot_score(query_embedding, embeddings).detach().cpu().numpy()[0]
+        similar_ix = np.argsort(cosine_scores)[::-1][:k]
+        return similar_ix, cosine_scores, cluster_embeddings
+
+    def run(self, query, k=10):
+        start_time = time.time()
+        print (f'Query: {query}')
+        indx, scores, cluster_embeddings = self.most_similar(query, k)
+        self.show(indx, scores, cluster_embeddings)
+        print()
+        print(f'Execution time: {time.time()-start_time}')
+
+
+kmeans_clustering_model = KmeansClustering()
+kmeans_clustering_model.run('Materials')
