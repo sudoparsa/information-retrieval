488ef961e56b2966a7e5ae8e0662735fc980f04c
sudoparsa <sp.hoseiny@gmail.com>
2022-07-22 23:38:12 +0430

    add elasticsearch

--- code/ranking.py
+++ code/ranking.py
@@ -1,7 +1,7 @@
 import json
 
 pagerank_result_path='models/Ranking/pagerank_result.json'
-hits_result_path='models/Ranking/hits.json'
+hits_result_path='models/Ranking/hits_result.json'
 
 pagerank_result = json.load(open(pagerank_result_path))
 hits_result = json.load(open(hits_result_path))
